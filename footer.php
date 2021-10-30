4b6300254477ac421da7a57fca7cf00d5cc67d13
mara <mara@maras-MacBook-Pro.local>
2021-10-30 16:54:03 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+
+<div class="wrapper" id="wrapper-footer">
+
+	<div class="<?php echo esc_attr( $container ); ?>">
+
+		<div class="row">
+
+			<div class="col-md-12">
+
+				<footer class="site-footer" id="colophon">
+
+					<div class="site-info">
+
+
+						<a href="https://www.instagram.com/fcseisa_08/">Seisa 08 auf Instagram</a><span class="sep"> | </span> We love Football  <span class="sep"> | </span> <a href="https://club.football.ch/club/spielbetrieb/aktuelle-spiele.aspx/v-875798">Seisa 08 auf club.football.ch</a>
+
+
+					</div><!-- .site-info -->
+
+				</footer><!-- #colophon -->
+
+			</div><!--col end -->
+
+		</div><!-- row end -->
+
+	</div><!-- container end -->
+
+</div><!-- wrapper end -->
+
+</div><!-- #page we need this extra closing tag here -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
+
---
+++ page-templates/template-sponsors.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Template Name: Sponsor Template
+ *
+ * Template für die Darstellung der Sponsoren
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+
+if ( is_front_page() ) {
+	get_template_part( 'global-templates/hero' );
+}
+?>
+
+<div class="wrapper" id="full-width-page-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 content-area" id="primary">
+
+				<main class="site-main" id="main" role="main">
+					
+					<?php
+					while ( have_posts() ) {
+						the_post();
+						get_template_part( 'loop-templates/content', 'page' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) {
+							comments_template();
+						}
+					}
+					?>
+
+					<?php
+
+					
+						$args = array(
+	    					'post_type'  => 'domi_sponsors_cpt',
+
+						);
+
+					?>
+					
+					<div class="row sponsor-row ">
+					<?php
+
+					$sponsors = get_posts( $args );
+					foreach ($sponsors as $sponsor ) {
+						 
+
+						?>
+						 
+							<div class="col-md-6 col-md-6 sponsor-col" >
+								<div class="card">
+									<h3><?php echo $sponsor->post_title;?></h3>
+							 		<div class="image-container">
+							 			<img src="<?=get_the_post_thumbnail_url($sponsor->ID, 'medium')?>" />
+							 		</div>	
+
+								</div>
+							</div>	
+
+						
+						<?php
+					}
+					?>
+					</div> <!-- sponsor row-->
+
+				</main><!-- #main -->
+
+			</div><!-- #primary -->
+
+		</div><!-- .row end -->
+
+	</div><!-- #content -->
+
+</div><!-- #full-width-page-wrapper -->
+
+<?php
+get_footer();
