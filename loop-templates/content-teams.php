c1825a202a2aa3ff9a75bc9031b3d0d6c58bf7d9
Dominique Müller <dominique_mueller@gmx.ch>
2021-11-28 17:31:17 +0100

    lighbox for mannschaften template

--- loop-templates/content-teams.php
+++ loop-templates/content-teams.php
@@ -21,14 +21,12 @@ global $team;
 				<h2><?php echo $team->post_title;?></h2>
 			</header><!-- .entry-header -->	
 			
-	 		<div class="image-container">
-	 			
-	 			<?php $teamlink = get_field('team_url', $team->ID); ?>
-	 			<a href="<?=$teamlink?>" target="_blank">
-	 				<img src="<?=get_the_post_thumbnail_url($team->ID, 'teampicture')?>" />
+	 		<div class="image-container"> 			
+
+	 			<a href="<?=get_the_post_thumbnail_url($team->ID, 'large')?>" data-rel="lightbox-image-0" data-rl_title="" data-rl_caption="" title="">
+	 				<img class="" src="<?=get_the_post_thumbnail_url($team->ID, 'teampicture')?>" />
 	 			</a>	
 	 		</div>	
-
 		</div>
 	</div>	
 
